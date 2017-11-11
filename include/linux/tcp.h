e8ca70f6d00fd2232a9bfaedba5763d95c2bbb94
mccore <edwardmccorry@aol.com>
2017-11-11 12:38:36 -0500

    took stuff out of tcp header file to see if it works

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -55,39 +55,6 @@ static inline unsigned int tcp_optlen(const struct sk_buff *skb)
 	return (tcp_hdr(skb)->doff - 5) * 4;
 }
 
-/* TCP Fast Open */
-#define TCP_FASTOPEN_COOKIE_MIN	4	/* Min Fast Open Cookie size in bytes */
-#define TCP_FASTOPEN_COOKIE_MAX	16	/* Max Fast Open Cookie size in bytes */
-#define TCP_FASTOPEN_COOKIE_SIZE 8	/* the size employed by this impl. */
-
-/* TCP Fast Open Cookie as stored in memory */
-struct tcp_fastopen_cookie {
-	union {
-		u8	val[TCP_FASTOPEN_COOKIE_MAX];
-#if IS_ENABLED(CONFIG_IPV6)
-		struct in6_addr addr;
-#endif
-	};
-	s8	len;
-	bool	exp;	/* In RFC6994 experimental option format */
-};
-
-/* This defines a selective acknowledgement block. */
-struct tcp_sack_block_wire {
-	__be32	start_seq;
-	__be32	end_seq;
-};
-
-struct tcp_sack_block {
-	u32	start_seq;
-	u32	end_seq;
-};
-
-/*These are used to set the sack_ok field in struct tcp_options_received */
-#define TCP_SACK_SEEN     (1 << 0)   /*1 = peer is SACK capable, */
-#define TCP_FACK_ENABLED  (1 << 1)   /*1 = FACK is enabled locally*/
-#define TCP_DSACK_SEEN    (1 << 2)   /*1 = DSACK was received from peer*/
-
 struct tcp_options_received {
 /*	PAWS/RTTM data	*/
 	long	ts_recent_stamp;/* Time we stored ts_recent (for aging) */
@@ -96,9 +63,9 @@ struct tcp_options_received {
 	u32	rcv_tsecr;	/* Time stamp echo reply        	*/
 	u16 	saw_tstamp : 1,	/* Saw TIMESTAMP on last packet		*/
 		tstamp_ok : 1,	/* TIMESTAMP seen on SYN packet		*/
-		dsack : 1,	/* D-SACK is scheduled			*/
+		unused : 1,	/* D-SACK is scheduled			THIS IS CHANGED TO UNUSED BY RPITCP*/
 		wscale_ok : 1,	/* Wscale seen on SYN packet		*/
-		sack_ok : 4,	/* SACK seen on SYN packet		*/
+		unused2 : 4,	/* SACK seen on SYN packet		THIS IS CHANGED TO UNUSED BY RPITCP*/
 		snd_wscale : 4,	/* Window scaling received from sender	*/
 		rcv_wscale : 4;	/* Window scaling to send to receiver	*/
 	u8	num_sacks;	/* Number of SACK blocks		*/
@@ -111,12 +78,6 @@ static inline void tcp_clear_options(struct tcp_options_received *rx_opt)
 	rx_opt->tstamp_ok = rx_opt->sack_ok = 0;
 	rx_opt->wscale_ok = rx_opt->snd_wscale = 0;
 }
-
-/* This is the max number of SACKS that we'll generate and process. It's safe
- * to increase this, although since:
- *   size = TCPOLEN_SACK_BASE_ALIGNED (4) + n * TCPOLEN_SACK_PERBLOCK (8)
- * only four options will fit in a standard TCP header */
-#define TCP_NUM_SACKS 4
 
 struct tcp_request_sock_ops;
 
@@ -184,15 +145,11 @@ struct tcp_sock {
 	struct u64_stats_sync syncp; /* protects 64bit vars (cf tcp_get_info()) */
 
  	u32	snd_una;	/* First byte we want an ack for	*/
- 	u32	snd_sml;	/* Last byte of the most recently transmitted small packet */
-	u32	rcv_tstamp;	/* timestamp of last received ACK (for keepalives) */
 	u32	lsndtime;	/* timestamp of last sent data packet (for restart window) */
 	u32	last_oow_ack_time;  /* timestamp of last out-of-window ACK */
 
 	u32	tsoffset;	/* timestamp offset */
 
-	struct list_head tsq_node; /* anchor in tsq_tasklet.head list */
-	unsigned long	tsq_flags;
 
 	/* Data for direct copy to user */
 	struct {
@@ -220,19 +177,6 @@ struct tcp_sock {
 	u16	advmss;		/* Advertised MSS			*/
 	u8	rate_app_limited:1,  /* rate_{delivered,interval_us} limited? */
 		unused:7;
-	u8	nonagle     : 4,/* Disable Nagle algorithm?             */
-		thin_lto    : 1,/* Use linear timeouts for thin streams */
-		thin_dupack : 1,/* Fast retransmit on first dupack      */
-		repair      : 1,
-		frto        : 1;/* F-RTO (RFC5682) activated in CA_Loss */
-	u8	repair_queue;
-	u8	do_early_retrans:1,/* Enable RFC5827 early-retransmit  */
-		syn_data:1,	/* SYN includes data */
-		syn_fastopen:1,	/* SYN includes Fast Open option */
-		syn_fastopen_exp:1,/* SYN includes Fast Open exp. option */
-		syn_data_acked:1,/* data in SYN is acked by SYN-ACK */
-		save_syn:1,	/* Save headers of SYN packet */
-		is_cwnd_limited:1;/* forward progress limited by snd_cwnd? */
 	u32	tlp_high_seq;	/* snd_nxt at the time of TLP retransmit. */
 
 /* RTT measurement */
@@ -247,12 +191,6 @@ struct tcp_sock {
 	u32	retrans_out;	/* Retransmitted packets out		*/
 	u32	max_packets_out;  /* max packets_out in last window */
 	u32	max_packets_seq;  /* right edge of max_packets_out flight */
-
-	u16	urg_data;	/* Saved octet of OOB data and control flags */
-	u8	ecn_flags;	/* ECN status bits.			*/
-	u8	keepalive_probes; /* num of allowed keep alive probes	*/
-	u32	reordering;	/* Packet reordering metric.		*/
-	u32	snd_up;		/* Urgent pointer		*/
 
 /*
  *      Options received (usually on last packet, some only on SYN packets).
@@ -285,28 +223,6 @@ struct tcp_sock {
 	u32	notsent_lowat;	/* TCP_NOTSENT_LOWAT */
 	u32	pushed_seq;	/* Last pushed seq, required to talk to windows */
 	u32	lost_out;	/* Lost packets			*/
-	u32	sacked_out;	/* SACK'd packets			*/
-	u32	fackets_out;	/* FACK'd packets			*/
-
-	/* from STCP, retrans queue hinting */
-	struct sk_buff* lost_skb_hint;
-	struct sk_buff *retransmit_skb_hint;
-
-	/* OOO segments go in this rbtree. Socket lock must be held. */
-	struct rb_root	out_of_order_queue;
-	struct sk_buff	*ooo_last_skb; /* cache rb_last(out_of_order_queue) */
-
-	/* SACKs data, these 2 need to be together (see tcp_options_write) */
-	struct tcp_sack_block duplicate_sack[1]; /* D-SACK block */
-	struct tcp_sack_block selective_acks[4]; /* The SACKS themselves*/
-
-	struct tcp_sack_block recv_sack_cache[4];
-
-	struct sk_buff *highest_sack;   /* skb just after the highest
-					 * skb with SACKed bit set
-					 * (validity guaranteed only if
-					 * sacked_out > 0)
-					 */
 
 	int     lost_cnt_hint;
 	u32     retransmit_high;	/* L-bits may be on up to this seqno */
@@ -322,10 +238,6 @@ struct tcp_sock {
 	u32	total_retrans;	/* Total retransmits for entire connection */
 
 	u32	urg_seq;	/* Seq of received urgent pointer */
-	unsigned int		keepalive_time;	  /* time before keep alive takes place */
-	unsigned int		keepalive_intvl;  /* time interval between keep alive probes */
-
-	int			linger2;
 
 /* Receiver side RTT estimation */
 	struct {
@@ -358,24 +270,7 @@ struct tcp_sock {
 	struct tcp_md5sig_info	__rcu *md5sig_info;
 #endif
 
-/* TCP fastopen related information */
-	struct tcp_fastopen_request *fastopen_req;
-	/* fastopen_rsk points to request_sock that resulted in this big
-	 * socket. Used to retransmit SYNACKs etc.
-	 */
-	struct request_sock *fastopen_rsk;
 	u32	*saved_syn;
-};
-
-enum tsq_flags {
-	TSQ_THROTTLED,
-	TSQ_QUEUED,
-	TCP_TSQ_DEFERRED,	   /* tcp_tasklet_func() found socket was owned */
-	TCP_WRITE_TIMER_DEFERRED,  /* tcp_write_timer() found socket was owned */
-	TCP_DELACK_TIMER_DEFERRED, /* tcp_delack_timer() found socket was owned */
-	TCP_MTU_REDUCED_DEFERRED,  /* tcp_v{4|6}_err() could not call
-				    * tcp_v{4|6}_mtu_reduced()
-				    */
 };
 
 static inline struct tcp_sock *tcp_sk(const struct sock *sk)
